37a7d3e1a84c4110a5e636da61e2aac87902f013
FabioVillar <63696039+FabioVillar@users.noreply.github.com>
2020-04-15 18:22:44 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+package trabalho;
+public class Main{
+    public static void main(String[] args){
+        Tabuleiro tab = new Tabuleiro();
+        System.out.println("Tabuleiro inicial:");
+        tab.imprimirtabuleiro();
+        CSVReader csv = new CSVReader();
+        csv.setDataSource("Trabalho-POO-2020---P1/blob/master/Entradas.csv");
+        String commands[] = csv.requestCommands();
+        int tamanho = commands.length;
+        int aux = 0;
+        while(aux < tamanho) {
+        	@SuppressWarnings("unused")
+			Movimentacao mov = new Movimentacao(tab, commands);
+        	aux ++;
+        }
+    }
+}
